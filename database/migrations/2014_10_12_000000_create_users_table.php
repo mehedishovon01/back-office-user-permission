9f84099281cea5a345a670baa92ded4f40416f97
mehedishovon01 <mehedishovon01@gmail.com>
2021-10-24 20:53:06 +0600

    Unique

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,12 +16,12 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('email')->unique();
+            $table->string('email', 255)->unique();
             $table->boolean('status')->default(1);
             $table->string('phone')->nullable();
             $table->string('gender')->nullable();
             $table->string('image')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->timestamp('email_verified_at')->nullable(); 
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
